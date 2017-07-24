6879bf03855f9a338f867d18e4e8383c0e3c0ca2
taeshik Hwang <ghjkghj@naver.com>
2017-07-24 16:18:37 +0900

    Action Creator Shortcuts

--- testing/src/components/comment_box.js
+++ testing/src/components/comment_box.js
@@ -1,34 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommentBox extends Component {
-  constructor(props) {
-    super(props);
+class CommentBox extends Component {
+	constructor(props) {
+		super(props);
 
-    this.state = { comment: '' }
-  }
+		this.state = { comment: '' };
+	}
 
-  handleChange(event) {
-    this.setState({ comment: event.target.value });
-  }
+	handleChange(event) {
+		this.setState({ comment: event.target.value });
+	}
 
-  handleSubmit(event) {
-    event.preventDefault();
+	handleSubmit(event) {
+		event.preventDefault();
 
-    this.setState({ comment: '' });
-  }
+		this.props.saveComment(this.state.comment);
+		this.setState({ comment: '' });
+	}
 
-  render() {
-    return(
-      <form
-        className="comment-box"
-        onSubmit={this.handleSubmit.bind(this)}
-      >
-        <textarea
-          value={this.state.comment}
-          onChange={this.handleChange.bind(this)}
-        />
-        <button action="submit">Submit</button>
-      </form>
-    );
-  }
+	render() {
+		return (
+			<form
+				onSubmit={this.handleSubmit.bind(this)}
+				className="comment-box"
+			>
+				<textarea
+					value={this.state.comment}
+					onChange={this.handleChange.bind(this)}
+				/>
+				<button action="submit">Submit Comment</button>
+			</form>
+		);
+	}
 }
+
+export default connect(null, actions)(CommentBox);
