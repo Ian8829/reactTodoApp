1652ad35b660e7df6c36a6b436d0087b3882cb0f
taeshik Hwang <ghjkghj@naver.com>
2017-07-24 17:37:40 +0900

    TestUtils Library

--- testing/test/test_helper.js
+++ testing/test/test_helper.js
@@ -1,5 +1,7 @@
 import jsdom from 'jsdom';
 import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 
 // Set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
@@ -7,6 +9,11 @@ global.window = global.document.defaultView;
 const $ = jquery(global.window);
 
 // build 'renderComponent' helper that should render a given react class
+function renderComponent(ComponentClass) {
+	const componentinstance = TestUtils.renderIntoDocument(<ComponentClass />);
+
+	return $(ReactDOM.findDOMNode(componentinstance)); // produces HTML
+}
 
 // Build helper for simulating events
 
