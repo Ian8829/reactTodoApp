db4a7fb25ff7488cfa7cc060854c90353b8dd655
taeshik Hwang <ghjkghj@naver.com>
2017-07-24 16:44:37 +0900

    Spec Failures After Code Change

--- testing/test/reducers/comments_test.js
+++ testing/test/reducers/comments_test.js
@@ -4,7 +4,7 @@ import { SAVE_COMMENT } from '../../src/actions/types';
 
 describe('Comment Reducer', () => {
 	it('handles action with unknown type', () => {
-		expect(commentReducer()).to.eql([]);
+		expect(commentReducer(undefined, {})).to.eql([]);
 	});
 
 	it('handles action of type SAVE_COMMENT', () => {
